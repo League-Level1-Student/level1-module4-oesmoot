fc78b9c3aa35ee498915148c881394e8a4f9a646
oesmo <oesmo@LaptopOfOwen>
2024-05-26 12:56:18 -0700

    going to start fortune teller

--- src/_03_typing_tutor/TypingTutor.java
+++ src/_03_typing_tutor/TypingTutor.java
@@ -1,17 +1,21 @@
 package _03_typing_tutor;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class TypingTutor {
+public class TypingTutor implements KeyListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
 	char currentLetter;
 public static void main(String[] args) {
-	
+	new TypingTutor().setup();
 }
 char generateRandomLetter() {
     Random r = new Random();
@@ -21,7 +25,37 @@ char generateRandomLetter() {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		currentLetter = generateRandomLetter();
-		JLabel label = new JLabel();
+		label.setText(String.valueOf(currentLetter));
+		label.setFont(label.getFont().deriveFont(28.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		frame.add(panel);
+		panel.add(label);
+		frame.pack();
+		frame.addKeyListener(this);
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char keyPressed = e.getKeyChar();
+		System.out.println("you typed: "+keyPressed);
+		if (keyPressed ==currentLetter) {
+			System.out.println("correct!");
+			panel.setBackground(Color.GREEN);
+		}
+		else {
+			System.out.println("incorrect!");
+			panel.setBackground(Color.red);
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		currentLetter = generateRandomLetter();
 		label.setText(String.valueOf(currentLetter));
 	}
 
--- src/_04_book_of_illusions/BookOfIllusions.java
+++ src/_04_book_of_illusions/BookOfIllusions.java
@@ -11,29 +11,58 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
 
 public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+	JFrame frame = new JFrame();
+	JLabel gideon;
+	JLabel isaac;
+	Boolean isGideonOnScreen = true;
 	public void run() {
 		// 2. make the frame visible
+		frame.setVisible(true);
 		// 3. set the size of the frame
+		frame.setSize(500,500);
 		// 4. find 2 images and save them to your project’s _06_book_of_illusions folder
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String illusion1 = "gideon.jpg";
+		String illusion2 = "gooditem.jpg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		gideon = loadImageFromComputer(illusion1);
+		isaac = loadImageFromComputer(illusion2);
 		// 8. add your JLabel to the frame
+		frame.add(gideon);
 		// 9. call the pack() method on the frame
+		frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("clicked!");
 		// 12. remove everything from the frame that was added earlier
+		if(isGideonOnScreen) {
+			frame.remove(gideon);
+			isGideonOnScreen = false;
+			frame.add(isaac);
+			JOptionPane.showMessageDialog(null, "OOOAAAHHHHHH!!!!!");
+		}
+		else {
+			frame.remove(isaac);
+			isGideonOnScreen = true;
+			frame.add(gideon);
+			JOptionPane.showMessageDialog(null, "awwww...");
+		}
+		frame.pack();
+		
 		// 13. load a new image like before (this is more than one line of code)
 		// 14. pack the frame
 	}
