ae3a4385076489fc20e2afa27f170fa29e81c5a5
league <karinabriellen@gmail.com>
2024-06-22 11:29:18 -0700

    starting slot machine

--- src/_09_whack_a_mole/WhackAMole.java
+++ src/_09_whack_a_mole/WhackAMole.java
@@ -1,11 +1,16 @@
 package _09_whack_a_mole;
 
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Random;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener {
@@ -20,6 +25,9 @@ JButton button6 = new JButton();
 JButton button7 = new JButton();
 JButton button8 = new JButton();
 JButton button9 = new JButton();
+int molesWhacked = 0;
+Date timeAtStart = new Date();
+int molesMissed = 0;
 
 public void setup() {
 	frame.add(panel);
@@ -34,7 +42,7 @@ public void setup() {
 	panel.add(button9);
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(250,140);
+	frame.setSize(300,140);
 	button1.addActionListener(this);
 	button2.addActionListener(this);
 	button3.addActionListener(this);
@@ -84,15 +92,65 @@ public void drawButtons() {
 	else if(moleNum == 8) {
 		button9.setText("MOLE");
 	}
+	
 }
 public static void main(String[] args) {
 	WhackAMole mole = new WhackAMole();
 	mole.setup();
 	mole.drawButtons();
 }
+private void playSound(String fileName) { 
+    AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+    sound.play();
+}
+private void endGame(Date timeAtStart, int molesWhacked) { 
+    Date timeAtEnd = new Date();
+    JOptionPane.showMessageDialog(null, "Your whack rate is "
+            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+                  + " moles per second.");
+}
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
 	JButton buttonPressed =(JButton) e.getSource();
+	if(buttonPressed.getText().equalsIgnoreCase("Mole")) {
+		playSound("9648ae0f-e14a-4d1d-8170-a41a752a3dff.wav");
+		this.drawButtons();
+		molesWhacked ++;
+		if(molesWhacked >= 10) {
+			endGame(timeAtStart,molesWhacked);
+		}
+	}
+	else {
+		molesMissed ++;
+		if(molesMissed == 1) {
+			speak("miss");
+		}
+		else if(molesMissed == 2) {
+			speak("again?");
+		}
+		else if(molesMissed ==3) {
+			speak("okay, i'll let you off this time, but be careful");
+		}
+		else if(molesMissed ==4) {
+			speak("okay, this is your last warning, you gotta get your act together or you will lose.");
+		}
+		else if(molesMissed == 5) {
+			speak("alright, that's it! i've had it with your shenannigans! I told you not to miss four whole times and you still kept going? you lose!");
+			endGame(timeAtStart,molesWhacked);
+		}
+                
+    }
+
+	
 }
+private void speak(String missComment) {
+	try {
+		Runtime.getRuntime().exec( "say " + missComment ).waitFor();
+	} catch (InterruptedException | IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+}
+
 }
---
+++ src/_10_slot_machine/SlotMachine.java
@@ -0,0 +1,9 @@
+package _10_slot_machine;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SlotMachine {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+}
