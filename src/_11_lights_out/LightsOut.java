e2a24534fc3bb4fbbaf3a24259b8ed4d7829dd36
league <league@imac-22.attlocal.net>
2024-07-13 11:29:37 -0700

    mhm

--- src/_11_lights_out/LightsOut.java
+++ src/_11_lights_out/LightsOut.java
@@ -1,9 +1,11 @@
 package _11_lights_out;
 
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -25,34 +27,38 @@ public class LightsOut implements MouseListener {
 
 		/** PART 1. CREATE YOUR LIGHT BOARD **/
 		//1. Make your gamePanel a 5x5 grid with setLayout(new GridLayout(5, 5));
+		gamePanel.setLayout(new GridLayout(5,5));
 		
-		
-			//2. Add 25 JLabels to your gamePanel (these are your lights)
-
-			//3. Use setText() to add a position number to each light (0-24).
-
-			//4. Set the background of each light to LIGHT_GRAY
-			// - you will also have to set the background to opaque.
-			// - Use light.setOpaque(true);
-
+			
+			for(int i = 0; i<25; i++) {
+				JLabel light = new JLabel();
+				gamePanel.add(light);
+				light.setText(String.valueOf(i+1));
+				light.setOpaque(true);
+				light.setBackground(Color.LIGHT_GRAY);
+				light.addMouseListener(this);
+			}
 			//5. Add a mouseListener to each light
 		
 		
 		//6. Add your panel to a frame
-
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(gamePanel);
 		//7. Set the size of the frame
-
+		frame.setSize(500,500);
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		/** PART 2: TOGGLE NEIGHBORING LIGHTS **/
 		// 1. Get the light that was clicked on `(JLabel) e.getSource`
-
+		JLabel source = (JLabel) e.getSource();
 		// 2. Get the number (position) of the light
-
+		source.getText();
 		// 3. Now use the makeMove method to code which lights turn on and off.
-
+		makeMove(Integer.valueOf(source.getText())-1);
 		// 4.Check if the player has won (e.g. all the lights are off)
 		// ---- HINT: use `getLightAtPosition` to get the light at each position
 		// ---------- use 'getBackground' to get the light color
